8227126f2e07fa15d2789ff0fcbfe546f92bcd58
Sitam <sitammeur@gmail.com>
2023-11-02 23:38:14 +0530

    somes docs and notebook folder added

--- src/logger.py
+++ src/logger.py
@@ -3,18 +3,24 @@ import os
 import logging
 from datetime import datetime
 
-# Create logs directory
-LOG_FILE = f"{datetime.now().strftime('%m-%d%Y-%H-%M-%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 
-os.makedirs(logs_path, exist_ok=True)
+# Function Definition to log the errors and warnings in the logs directory of the project folder
+def create_logger():
+    """
+    Create logger for the program.
+    """
+    # Create logs directory
+    LOG_FILE = f"{datetime.now().strftime('%m-%d%Y-%H-%M-%S')}.log"
+    logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 
-# Create log file path
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+    os.makedirs(logs_path, exist_ok=True)
 
-# Configure logging
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
+    # Create log file path
+    LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+    # Configure logging
+    logging.basicConfig(
+        filename=LOG_FILE_PATH,
+        format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )

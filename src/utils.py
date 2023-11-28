a7b926ac6709054b1d87c892097325f7dbf537ce
Sitam <sitammeur@gmail.com>
2023-11-29 01:00:26 +0530

    evaluate model method added

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pickle
 
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 
 # Exception handling imports
 from src.exception import CustomExceptionHandling
@@ -36,9 +37,41 @@ def save_object(obj, file_path):
         raise CustomExceptionHandling(e, sys) from e
 
 
-def evaluate_model(model, X_test, y_test):
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluate the performance of different models on the given training and testing data.
+
+    Parameters:
+    X_train (array-like): Training data features.
+    y_train (array-like): Training data labels.
+    X_test (array-like): Testing data features.
+    y_test (array-like): Testing data labels.
+    models (dict): Dictionary of models to evaluate, where the keys are model names and the values are model objects.
+
+    Returns:
+    dict: A dictionary containing the model names as keys and the corresponding R-squared scores on the testing data as values.
+    """
     try:
-        pass
+        # Creating a dictionary to store the model names and their R-squared scores
+        report = {}
+
+        for i in range(len(list(models))):
+            # Fitting the model on the training data
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+
+            # Predicting the labels for the training and testing data
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Calculating the R-squared scores for the training and testing data
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            # Storing the scores in the report dictionary
+            report[list(models.keys())[i]] = test_score
+
+        return report
 
     except Exception as e:
         # Handling the exception
